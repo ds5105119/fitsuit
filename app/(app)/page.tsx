71f7cc4dd65a0d71f0ca89a1adcc83b506810eba
ds5105119 <ds5105119@gmail.com>
2025-12-21 18:19:15 +0900

    메인 페이지 변경

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -16,8 +16,8 @@ export default function HomePage() {
 
   return (
     <LenisProvider>
-      <main className="min-h-screen bg-black text-white">
-        <section className="relative min-h-screen overflow-hidden">
+      <main className="min-h-screen">
+        <section className="relative min-h-screen overflow-hidden bg-black">
           <video autoPlay muted loop playsInline className="absolute inset-0 h-full w-full object-cover">
             <source src="/images/main-video.mp4" type="video/mp4" />
           </video>
@@ -25,10 +25,10 @@ export default function HomePage() {
           <div className="absolute h-full w-full bg-linear-to-b from-black/0 via-black/0 to-black" />
         </section>
 
-        <section className="mx-auto py-36 md:py-56">
+        <section className="mx-auto py-36 pb-24 lg:pb-36 lg:pt-56 bg-black">
           <div className="grid gap-10 max-w-6xl px-6 items-center mx-auto">
             <div className="space-y-5">
-              <p className="text-base font-medium leading-relaxed sm:text-sm">Vision</p>
+              <p className="text-base font-medium leading-relaxed text-white sm:text-sm">Vision</p>
               <p className="text-5xl font-light leading-tight text-white sm:text-7xl font-instrument">
                 We create bespoke garments that merge design vision with master craftsmanship.
               </p>
@@ -41,7 +41,7 @@ export default function HomePage() {
           </div>
         </section>
 
-        <section className="w-full bg-white text-black">
+        <section className="w-full hidden">
           <div className="mx-auto flex flex-col gap-4 px-6">
             <div className="w-full grid-cols-3 gap-12 pt-12 px-6 hidden lg:grid" onMouseLeave={() => setCardHovered(null)}>
               {cardItems.map((item, idx) => (
@@ -85,16 +85,20 @@ export default function HomePage() {
               <CarouselPrevious variant="ghost" className="left-2 text-white top-1/2 -translate-y-1/2" />
               <CarouselNext variant="ghost" className="right-2 text-white top-1/2 -translate-y-1/2" />
             </Carousel>
+          </div>
+        </section>
 
-            <div className="max-w-3xl mx-auto text-lg pt-12 flex flex-col items-center">
+        <section className="w-full text-black py-36">
+          <div className="mx-auto flex flex-col gap-4 px-6">
+            <div className="max-w-3xl mx-auto text-lg flex flex-col items-center">
               <div>
                 <p className="text-base font-medium leading-relaxed sm:text-sm text-center">Custom Made</p>
-                <p className="text-3xl font-bold leading-relaxed sm:text-4xl text-center">
+                <p className="text-3xl font-bold leading-relaxed sm:text-4xl text-center pt-4">
                   &lsquo;가치&rsquo;를 찾는 여정,
                   <br />
                   대한민국 양복 명장이 도와드리겠습니다.
                 </p>
-                <p className="text-sm lg:text-base leading-relaxed">
+                <p className="text-sm lg:text-base leading-relaxed pt-6">
                   당신만을 위한 여정은 마스터 테일러와의 창의적인 대화에서 시작됩니다. 희소한 고급 원단들 중에서 상상할 수 있는 모든 생동감 있는 색상과 패턴을
                   함께 선택하며, 당신이 그리는 이상적인 모습을 하나씩 구체화해 나갑니다. 예상치 못한 안감, 호화로운 단추, 그리고 완성도를 높여 주는 각종 마감
                   디테일까지 — 모든 요소가 당신의 꿈을 실현하기 위한 하나의 과정으로 세심하게 고려됩니다. 비스포크 경험 전반에 걸쳐 마스터 테일러는 오랜 시간
@@ -120,21 +124,8 @@ export default function HomePage() {
           </div>
         </section>
 
-        <section className="w-full bg-white text-black pt-36">
+        <section className="w-full bg-white text-black">
           <div className="flex flex-col space-y-48">
-            {bespokeSteps.slice(0, 2).map((step, idx) => (
-              <div key={step.title} className="grid px-6 lg:px-24 items-center gap-10 mx-auto lg:grid-cols-5">
-                <div className={cn("relative aspect-4/3 w-full overflow-hidden lg:col-span-3", idx % 2 === 1 && "lg:order-2")}>
-                  <Image alt={step.title} src={step.image} fill priority className="object-cover" />
-                </div>
-
-                <div className={cn("flex flex-col gap-6 lg:col-span-2", idx % 2 === 1 && "lg:order-1")}>
-                  <p className="text-sm leading-relaxed text-black sm:text-base font-bold">{step.title}</p>
-                  <p className="text-sm leading-relaxed text-black/70 sm:text-base whitespace-pre-line">{step.subtitle}</p>
-                </div>
-              </div>
-            ))}
-
             <div>
               <div className="relative w-full aspect-22/9 lg:aspect-28/9 overflow-hidden">
                 <Image alt="이정구 명장 도쿄 우승" src="/images/도쿄 인증서.png" fill priority className="object-cover" />
@@ -145,44 +136,38 @@ export default function HomePage() {
                 <p>도쿄 국제기능올림픽</p>
               </div>
             </div>
-
-            {bespokeSteps.slice(2, 4).map((step, idx) => (
-              <div key={step.title} className="grid px-6 lg:px-24 items-center gap-10 mx-auto lg:grid-cols-5">
-                <div className={cn("relative aspect-4/3 w-full overflow-hidden lg:col-span-3", idx % 2 === 1 && "lg:order-2")}>
-                  <Image alt={step.title} src={step.image} fill priority className="object-cover" />
-                </div>
-
-                <div className={cn("flex flex-col gap-6 lg:col-span-2", idx % 2 === 1 && "lg:order-1")}>
-                  <p className="text-sm leading-relaxed text-black sm:text-base font-bold">{step.title}</p>
-                  <p className="text-sm leading-relaxed text-black/70 sm:text-base whitespace-pre-line">{step.subtitle}</p>
-                </div>
-              </div>
-            ))}
           </div>
         </section>
 
-        <section className="w-full bg-white text-black pt-36">
-          {" "}
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 px-6">
-            <div className="order-2 lg:order-1 lg:col-span-1">
-              <div className="relative aspect-square w-full overflow-hidden">
-                <Image alt="원단" src="/images/fabric1.png" fill priority className="object-cover" />
-              </div>
-
-              <div className="pt-4 text-xs lg:text-sm">
-                <p className="font-bold">Super 180's</p>
-                <p>PIACENZA 1733</p>
-              </div>
+        <section className="mx-auto py-36 lg:py-56">
+          <div className="grid gap-10 max-w-6xl px-6 items-center mx-auto">
+            <div className="space-y-5">
+              <p className="text-base font-medium leading-relaxed sm:text-sm">Vision</p>
+              <p className="text-5xl font-light leading-tight sm:text-7xl font-instrument">
+                We create bespoke garments that merge design vision with master craftsmanship.
+              </p>
+              <p className="text-sm leading-relaxed text-black/70 sm:text-base lg:w-[70%] break-keep">
+                비스포크 수트는 기성복이나 일반 맞춤(MTM)과는 차원이 다릅니다. 고객의 직업과 자세, 생활 습관, 그리고 취향까지 세밀하게 반영하여 전용 패턴을
+                새롭게 제작하며, 원단과 안감, 단추, 세부 디자인까지 모두 고객이 직접 선택할 수 있습니다. 그 결과 완성되는 수트는 세상에 단 하나뿐인, 오직 한
+                사람만을 위한 작품입니다.
+              </p>
             </div>
-            <div className="order-1 lg:order-2 lg:col-span-2">
-              <div className="relative aspect-3/4 w-full overflow-hidden bg-gray-100">
-                <Image alt="모델" src="/images/suit1.png" fill priority className="object-cover" />
-              </div>
 
-              <div className="pt-4 text-xs lg:text-sm">
-                <p className="font-bold">스트라입 슈트</p>
-                <p>네이비 핀</p>
-              </div>
+            <div className="w-full flex flex-col space-y-36 pt-12">
+              {bespokeSteps.map((step, idx) => (
+                <div key={step.title} className={cn("lg:w-[75%] grid items-center gap-10 lg:gap-0 lg:grid-cols-9", idx % 2 == 0 && "ml-auto")}>
+                  <div className={cn("flex flex-col gap-6 lg:col-span-5 lg:pr-10")}>
+                    <div className="flex items-start">
+                      <p className="text-3xl leading-relaxed sm:text-5xl font-instrument ">{step.title}</p>
+                      <p className="text-sm sm:font-base font-instrument italic">(0{idx})</p>
+                    </div>
+                    <p className="text-sm leading-relaxed text-black/70 sm:text-base whitespace-pre-line">{step.subtitle}</p>
+                  </div>
+                  <div className={cn("relative aspect-square w-full overflow-hidden lg:col-span-4")}>
+                    <Image alt={step.title} src={step.image} fill priority className="object-cover" />
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </section>
